147d8fb1370c3ec990ec965daad67069b696ccfe
scottmclaughlin88 <110846892+scottmclaughlin88@users.noreply.github.com>
2023-02-01 19:42:15 -0600

    Cleaned up notes.

--- plagariasm_checker.py
+++ plagariasm_checker.py
@@ -1,30 +1,8 @@
 #Write a function that will find the second largest in a list
 from time import time
-#pass ignores the function
+
 def basic_list(list1):
     pass
-#1nd method
-    # list1.sort()
-    # return list1[-2]
-
-#2nd method    
-    # largest = max(list1)
-    # list1.remove(largest)
-    # return max(list1)
-    
-# list1 = [1,-3,100,1000,500]
-# print(basic_list(list1))
-
-#How long does it take this function to run?
-#Sorting takes a lot of time.  Move lots of data around.  Overkill for a basic scenario.
-
-#Given two strings, find if there is a sequence of at least 5 words in both strings
-#Use case: checking for plagiarism.
-
-#Create a fuction that takes in two variables
-#define the string
-#loop through the words until you find a match
-#return the results
 
 def timer_func(func):
     def wrap_func(*args, **kwargs):
@@ -40,7 +18,7 @@ def matching_words(string1, other_strings):
     string1 = string1.lower()
 #Find all of the 5 word combinations in string1
     string1 = string1.split()
-#range what variable starts.  -1 changes will decrease by 1.
+
     for current_length in range(len(string1)-1, 0, -1):
         for start_position in range(len(string1)-current_length -1):
             words = string1[start_position:start_position + current_length]
@@ -54,17 +32,13 @@ def matching_letters(string1, string2):
     string1 = string1.lower()
     string2 = string2.lower()
 #Find all of the 5 word combinations in string1
-    # for i in
+
     for current_length in range(len(string1)-1, 0, -1):
         for start_position in range(len(string1)-current_length -1):
             letters = string1[start_position:start_position + current_length]
             if letters in string2:
                 return letters
-#abcde - This is all the letters of our str1, using a string splice ':'.
 
-#Never define parameters in the function, defeats the purpose.  
-# string1 = "The quick brown blue dark fox jumped over the lazy dog fence"
-# string2 = "Whales are really big and the quick brown blue dark fox jumped past everything and it was marvelous!"
 def get_longest_matching_words(filename1,other_files):
     other_strings = []
     file1  = open(filename1, 'r')
@@ -74,6 +48,3 @@ def get_longest_matching_words(filename1,other_files):
         string2 = file2.read()
         other_strings.append(string2)
     return matching_words(string1, other_strings)
-
-# result = get_longest_matching_words('plag_sample1.txt','plag_sample2.txt')
-# print(result)
--- test_gui.py
+++ test_gui.py
@@ -1,16 +1,8 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
-from weather_project import get_days_above_temperatures,get_days_below_temperatures
 from plagariasm_checker import get_longest_matching_words
 
-
-#
-# def get_days_above():
-#     label2.config(text=get_days_above_temperatures(entry1.get()))
-
-# def get_days_below():
-#     min_result.config(text=get_days_below_temperatures(min_entry.get()))
 def add_file():
     global files_added
     files_added += 1
@@ -19,7 +11,7 @@ def add_file():
     file2_label = Label(new_file_frame,text=f'File {files_added}',font=('Arial',20))
     file2_label.pack(side=LEFT)
 
-    #Add input box
+#Add input box
     file2_entry = Entry(new_file_frame)
     file2_entry.pack(side=LEFT)
 
@@ -31,6 +23,7 @@ def add_file():
     button_analyze_file = Button(new_file_frame,text='Analyze',command=lambda: analyze_file(file2_entry.get()))
     button_analyze_file.pack(side=LEFT)
 
+#Get matching words
 def get_matching_words():
     try:
         files = []
@@ -77,6 +70,7 @@ def browseFile(file2_entry):
     file2_entry.delete(0,END)
     file2_entry.insert(0,fileparts[-1])
 
+#Analyze top 3 most popular words used.
 def analyze_file(filename):
     filler_words = []
     with open('filler_words.txt','r') as file:
@@ -104,41 +98,10 @@ window.title('Data Analysis')
 files_added = 1
 file_widgets = []
 
-# frame = Frame(window)
-# frame.pack()
-
 plagiarism_frame1 = Frame(window)
 plagiarism_frame1.pack(side=TOP)
 plagiarism_frame2 = Frame(window)
 plagiarism_frame2.pack(side=TOP)
-
-
-#Adding objects
-# label1 = Label(window,text='Days above temp',font=('Arial',30))
-# label1.pack()
-# label2 = Label(window,text='',font=('Arial',30))
-# label2.pack()
-
-# #Add input box
-# entry1 = Entry(window)
-# entry1.pack()
-
-# #Create a button
-# button1 = Button(window,text='Get max',command=get_days_above)
-# button1.pack()
-
-# min_label1 = Label(window,text='Days below',font=('Arial',30))
-# min_label1.pack()
-# min_result = Label(window,text='',font=('Arial',30))
-# min_result.pack()
-
-# #Add input box
-# min_entry = Entry(window)
-# min_entry.pack()
-
-# #Create a button
-# get_min = Button(window,text='Get min',command=get_days_below)
-# get_min.pack()
 
 file1_label = Label(plagiarism_frame1,text='File 1',font=('Arial',20))
 file1_label.pack(side=LEFT)
@@ -154,23 +117,6 @@ button_analyze_file.pack(side=LEFT)
 button_explore.pack(side=LEFT)
 button_add_file = Button(window,text='Add file',command=add_file)
 button_add_file.pack()
-
-
-#Find most common word
-# button_common_word = Button(plagiarism_frame1,text='Common word',command=get_common_word)
-# button_common_word.pack()
-
-# file2_label = Label(plagiarism_frame2,text='File 2',font=('Arial',20))
-# file2_label.pack(side=LEFT)
-
-# #Add input box
-# file2_entry = Entry(plagiarism_frame2)
-# file2_entry.pack(side=LEFT)
-
-# button_explore2 = Button(plagiarism_frame2,
-#                         text = "Browse Files",
-#                         command = browseFiles2)
-# button_explore2.pack(side=LEFT)
 
 #Create a button
 find_pl = Button(window,text='Get plagiarism',command=get_matching_words)
